7308101b7ae392c25ce41258bb1b6477ffba99c7
AnjaliCodeCrafter <119666988+Anjali211003@users.noreply.github.com>
2025-01-28 14:24:45 +0530

    Delete frontend directory

--- frontend/postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss')({
-      config: './tailwind.config.js', // Ensure this path points to your Tailwind configuration file
-    }),
-    require('autoprefixer'),
-  ],
-};
